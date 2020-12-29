274c5395bbf97a87732e648152d9a421fcc57817
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-29 12:36:46 +0000

    calculator can subtract 2 numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,8 +1,12 @@
 require 'calculator'
 
 describe Calculator do
-  it "can preform multiplication" do
+  it "can preform addition" do
     expect(subject.add(2, 2)).to eq 4
+  end
+
+  it "can preform subtraction" do
+    expect(subject.subtract(2, 2)).to eq 0
   end
 
 end
