c9bac6ce088c6df8b637418a7b03e633a80f2f06
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-29 12:47:46 +0000

    move functionality of the prettyfied string to seperate class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,16 +1,12 @@
+require_relative './pretty.rb'
 class Calculator 
 
     def add(number_1, number_2)
-      pretty_string(number_1 + number_2)
+      Pretty.new.output_string(number_1 + number_2)
     end
 
     def subtract(number_1, number_2)
-      pretty_string(number_1 - number_2)
+      Pretty.new.output_string(number_1 - number_2)
     end
-
-    private
-
-    def pretty_string(answer)
-      "Your number is #{ answer }"
-    end
+    
 end
