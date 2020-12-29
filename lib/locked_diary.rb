be10eabd03de3145746418f1bfedc2b1e5528f12
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-29 14:59:47 +0000

    diary is unlocked in the LockedDiary class

---
+++ lib/locked_diary.rb
@@ -0,0 +1,13 @@
+class LockedDiary
+
+  attr_reader :is_it_locked
+
+  # def initialize
+  #   @is_it_locked = true
+  # end
+
+  def unlock
+    @is_it_locked = false
+  end
+
+end
--- spec/locked_diary_spec.rb
+++ spec/locked_diary_spec.rb
@@ -1,5 +1,14 @@
 require 'locked_diary'
 
-describe 'LockedDiary' do
+describe LockedDiary do
+
+  it "allows a user to unlock the diary" do
+    subject.unlock
+    expect(subject.is_it_locked).to eq false
+  end
+
+  # it "is locked upon creation" do
+  #   expect(subject.is_it_locked).to eq true
+  # end
 
 end
