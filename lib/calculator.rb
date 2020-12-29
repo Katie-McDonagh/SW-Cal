9ae134512754b4e3f4afda019d8205d4a56304cf
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-29 12:40:55 +0000

    refactor pretty string into a private method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,16 @@
 class Calculator 
 
     def add(number_1, number_2)
-      "Your number is #{ number_1 + number_2 }"
+      pretty_string(number_1 + number_2)
     end
 
     def subtract(number_1, number_2)
-      "Your number is #{ number_1 - number_2 }"
+      pretty_string(number_1 - number_2)
+    end
+
+    private
+
+    def pretty_string(answer)
+      "Your number is #{ answer }"
     end
 end
