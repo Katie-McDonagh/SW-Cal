400e40fa1f0c19ba5045525c9f96dd1345946a88
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-12-29 14:29:48 +0000

    user is able to unlock and lock the diary

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,6 +1,18 @@
 class SecretDiary 
 
-  def locked?
-    true
+  attr_reader :is_it_locked
+
+  def initialize 
+    @is_it_locked = true
+
   end
+
+  def lock
+    @is_it_locked = true
+  end
+
+  def unlock
+    @is_it_locked = false
+  end
+
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -2,7 +2,12 @@ require 'secret_diary'
 
 describe SecretDiary do
   it "is automatically locked when created" do
-    expect(subject.locked?).to eq true
+    expect(subject.is_it_locked).to eq true
+  end
+
+  it "can be unlocked at any time" do
+    subject.unlock
+    expect(subject.is_it_locked).to eq false
   end
 
 end
